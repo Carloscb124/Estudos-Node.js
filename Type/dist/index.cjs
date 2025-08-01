b0328229e08675fd0b2945e85d3b7bdc99a36574
Carlos Eduardo <cb473355@gmail.com>
2025-08-01 19:11:45 -0300

    type

---
+++ Type/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "cb473355@gmail.com";
+  console.log(myEmail);
+}
+main();
---
+++ Type/src/index.ts
@@ -0,0 +1,9 @@
+function main() {
+    let myEmail: string = "cb473355@gmail.com";
+    console.log(myEmail);
+};
+
+main();
+
+// para transcrever para javaScript usar npx tsc src/index.ts
+// criar arquivo tsconfig para mudar tudo de uma vez npx tsc --init
